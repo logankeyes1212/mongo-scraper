1c8c4f49481bd1be5431c87cfd90cd67217e9bfa
Logan <logankeyes1212@gmail.com>
2019-03-21 21:40:40 -0500

    test

--- server.js
+++ server.js
@@ -9,24 +9,9 @@ var PORT = 3000;
 var app = express();
 // var MONGODB_URI = process.env.MONGODB_URI || "mongodb://heroku_xt011v1w:password123456789@ds121026.mlab.com:21026/heroku_xt011v1w";
 // mongoose.connect(MONGODB_URI);
-// var MONGODB_URI = process.env.MONGODB_URI || ("mongodb://localhost/nytimes", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || ("mongodb://localhost/nytimes", { useNewUrlParser: true });
 
-// mongoose.connect(MONGODB_URI);
-const  MongoClient  = require("mongodb");
-const uri = ('mongodb://localhost:nytimes');  // mongodb://localhost - will fail
-
-(async function() {
-  try {
-
-    const client = await MongoClient.connect(uri,{ useNewUrlParser: true });
-    // ... anything
-
-    client.close();
-  } catch(e) {
-    console.error(e)
-  }
-
-})()
+mongoose.connect(MONGODB_URI);
 
 // mongoose.connect(MONGODB_URI, function (error) {
 //   if (error) console.error(error);
